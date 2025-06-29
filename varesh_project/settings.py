b8f76d16789cd9ed62ffc09507b5fcac07c60b7f
alfa <logicalfa.mail@gmail.com>
2025-06-29 17:14:13 +0330

    install psycopg2-binary & fix settings

--- varesh_project/settings.py
+++ varesh_project/settings.py
@@ -92,8 +92,12 @@ AUTH_USER_MODEL = 'account_module.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'varesh_db',
+        'USER': 'alfa',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
